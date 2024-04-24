585417a6e9101d57772e75e1523e9c098cd04dbd
Marcelkress <Marcel@kress.dk>
2024-04-24 13:17:45 +0200

    Random environment

--- Assets/Scripts/Marcel/RandomEnvironment.cs
+++ Assets/Scripts/Marcel/RandomEnvironment.cs
@@ -4,35 +4,37 @@ using UnityEngine;
 
 public class RandomEnvironment : MonoBehaviour
 {
+    private float addToZ = 0;
+    private float lastZ = 0;
+    private int smallHouseLength = 0;
+    private int bigHouseLenght = 0;
+    private bool bigCity = false;
+    private List<GameObject> RoadChunks = new List<GameObject>();
 
-    [SerializeField] private int removeDistance = 10;
-    [SerializeField] private int activateDistance = 100;
+    [SerializeField] private int instantiateDistance = 1;
     [SerializeField] private List<GameObject> smallHouses = new List<GameObject>();
     [SerializeField] private List<GameObject> bigHouses = new List<GameObject>();
     [SerializeField] private float bigHouseThreshold = 250;
+    [SerializeField] private float removeDistance = 50;
+    [SerializeField] private GameObject environment;
 
-    private List<GameObject> RoadChunks = new List<GameObject>();
 
-    private float addToZ = 0;
-    private float lastZ = 0;
-
-    private int smallHouseLength = 0;
-    private int bigHouseLenght = 0;
-
-    private bool bigCity = false;
-
-    // Start is called before the first frame update
     void Start()
     {
+        foreach(Transform child in environment.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
         bigCity = false;
         lastZ = transform.position.z;
-        addToZ = 2.383068f; // The distance between the chunks
+
+        addToZ = 23.83068f; // The distance between the chunks
 
         smallHouseLength = smallHouses.Count;
         bigHouseLenght = bigHouses.Count;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(transform.position.z > bigHouseThreshold)
@@ -40,17 +42,38 @@ public class RandomEnvironment : MonoBehaviour
             bigCity = true;
         }
 
-        if(transform.position.z > lastZ + addToZ)
+        if(transform.position.z - lastZ > instantiateDistance )
         {
-            lastZ = transform.position.z;
-            InstantiateChunk(new Vector3(0, 0, lastZ));
+            Vector3 position = new Vector3(0, 0, lastZ + addToZ);
+
+            GameObject prefabtoInstantiate;
+
+            if(bigCity)
+                prefabtoInstantiate = bigHouses[Random.Range(0, bigHouseLenght)];
+            
+            else
+                prefabtoInstantiate = smallHouses[Random.Range(0, smallHouseLength)];
+            
+            GameObject newChunk = Instantiate(prefabtoInstantiate, position, Quaternion.identity);
+            newChunk.transform.parent = environment.transform;
+            RoadChunks.Add(newChunk);
+            lastZ = newChunk.transform.position.z;
         }
 
+        RemoveChunks();
     }
 
-    private void InstantiateChunk(Vector3 position)
+    private void RemoveChunks()
     {
-        
+        if(RoadChunks.Count == 0)
+            return;
+
+        if(RoadChunks[0].transform.position.z < transform.position.z - removeDistance)
+        {
+            GameObject toRemove = RoadChunks[0];
+            RoadChunks.RemoveAt(0);
+            Destroy(toRemove);
+        }
     }
 
 }
