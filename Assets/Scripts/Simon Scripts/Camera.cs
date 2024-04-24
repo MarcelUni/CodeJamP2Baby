87ab09542a32a0d8b61cf3ec86bca28d0388f6d6
Bob-Ross-Codes <simon.hesthaven@gmail.com>
2024-04-24 09:11:05 +0200

    Camera og controller

--- Assets/Scripts/Simon Scripts/Camera.cs
+++ Assets/Scripts/Simon Scripts/Camera.cs
@@ -4,24 +4,19 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    #region Variables
-    
-        private Vector3 _offset;
-        [SerializeField] private Transform target;
-        [SerializeField] private float smoothTime;
-        private Vector3 _currentVelocity = Vector3.zero;
-        
-    #endregion
-    
-    #region Unity callbacks
-    
-        private void Awake() => _offset = transform.position - target.position;
+    private Vector3 _offset;
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothTime;
+    private Vector3 _currentVelocity = Vector3.zero;
 
-        private void LateUpdate()
-        {
-            Vector3 targetPosition = target.position + _offset;
-            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity, smoothTime);
-        }
-        
-    #endregion
+    private void Awake()
+    {
+        _offset = transform.position - target.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = target.position + _offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity, smoothTime);
+    }
 }
