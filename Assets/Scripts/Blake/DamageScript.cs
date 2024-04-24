94bc6a93fb340d13a9275605f6e05764c6b29479
bonkborban <159906295+bonkoborbano@users.noreply.github.com>
2024-04-24 16:46:55 +0200

    Update DamageScript.cs

--- Assets/Scripts/Blake/DamageScript.cs
+++ Assets/Scripts/Blake/DamageScript.cs
@@ -9,39 +9,23 @@ public class DamageScript : MonoBehaviour
     string[] smallVehicles = {"small_car(Clone)", "sport_car(Clone)", "taxi(Clone)", "blue_car(Clone)", "Hatchback(Clone)", "Sedan(Clone)"};
     string[] largeVehicles = {"truck(Clone)", "small_bus(Clone)", "bus(Clone)"};
     
-    public static DamageScript instance;
-
-    void Awake()
+    private void OnCollisionEnter(Collision collision)
     {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-            
-    }
-    void VehicleDamage(Collision collision)
-    {
-        
-        if(collision.gameObject.name == smallVehicles[0] || 
-           collision.gameObject.name == smallVehicles[1] || 
-           collision.gameObject.name == smallVehicles[2] || 
-           collision.gameObject.name == smallVehicles[3] || 
-           collision.gameObject.name == smallVehicles[4] || 
-           collision.gameObject.name == smallVehicles[5])
+        if(collision.gameObject.name == "small_car(Clone)" || 
+           collision.gameObject.name == "sport_car(Clone)" || 
+           collision.gameObject.name == "taxi(Clone)" || 
+           collision.gameObject.name == "blue_car(Clone)" || 
+           collision.gameObject.name == "Hatchback(Clone)" || 
+           collision.gameObject.name == "Sedan(Clone)")
         { 
-            GameManager.instance.RemoveHP();
+            GameManager.instance.RemoveHP(1);
             Debug.Log("Life lost");
         }
-        else if (collision.gameObject.name == largeVehicles[0] || 
-                 collision.gameObject.name == largeVehicles[1] || 
-                 collision.gameObject.name == largeVehicles[2])
+        else if (collision.gameObject.name == "truck(Clone)" || 
+                 collision.gameObject.name == "small_bus(Clone)" || 
+                 collision.gameObject.name == "bus(Clone)")
         {
-            GameManager.instance.RemoveHP();
+            GameManager.instance.RemoveHP(2);
             Debug.Log("Two Lives lost");
         }
     }
